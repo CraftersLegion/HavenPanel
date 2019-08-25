f72438dac95f9469c09cdc6cc1fd88fb530bd080
Elxafil <daniel.alexander@randamonium.com>
2019-08-25 21:36:07 +1200

    Added Initial Header.php
    Created initial header file to load configuration and functions.

---
+++ assets/header.php
@@ -0,0 +1,18 @@
+<?php
+
+	// Load Configuration
+	require_once "config.php";
+	
+	// Load Functions
+	require_once "functions.php";
+	
+?>
+<HTML lang="EN">
+
+	<HEAD>
+	
+	</HEAD>
+	
+	<BODY>
+	
+		
