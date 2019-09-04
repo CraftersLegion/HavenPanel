eb896461f176997a06add7dcaa5c52ab7ebb865f
Elxafil <daniel.alexander@randamonium.com>
2019-09-05 00:30:23 +1200

    Added Initial Assets

--- assets/header.php
+++ assets/header.php
@@ -20,10 +20,10 @@
 		<title>Haven Panel & IDE</title>
 		
 		<!-- Bootstrap CSS -->
-		<link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+		<link href="./assets/css/bootstrap.css" rel="stylesheet">
 		
 		<!-- Font Awesome CSS -->
-		<link href="/assets/css/font-awesome.min.css" rel="stylesheet">
+		<link href="./assets/css/font-awesome.css" rel="stylesheet">
 		
 		<!-- Custom CSS -->
 		<link href="./assets/css/admin.css" rel="stylesheet">
@@ -34,10 +34,27 @@
 	
 		<nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
 			<a class="navbar-brand col-sm-3 col-md-2 mr-0" href="<?php echo get_url(); ?>">Haven Realms Panel & IDE</a>
-			<input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
 			<ul class="navbar-nav px-3">
 				<li class="nav-item text-nowrap">
 					<a class="nav-link" href="#">Sign out</a>
 				</li>
 			</ul>
 		</nav>
+		
+		<div class="container-fluid">
+			<div class="row">
+			
+				<!-- Sidebar -->
+				<div class="sidebar-sticky">
+					<ul class="nav flex-column">
+						<li class="nav-item">
+							<a class="nav-link active" href="#"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link active" href="./users.php"><i class="fas fa-users"></i> Users</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link active" href="./users.php"><i class="fas fa-server"></i> Servers</a>
+						</li>
+					</ul>
+				</div>	
--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 
 	// Load Header
 	require "./assets/header.php";
+
+?>
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4"><div style="position: absolute; inset: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;" class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2"><i class="fas fa-tachometer-alt"></i> Dashboard</h1>
+    </div>
+	<div class="row card-group">
+		<div class="card bg-primary col-md-3">
+			<div class="card-title"><h3><i class="fas fa-lg fa-server"></i> 0 Servers</div>
+		</div>
+	</div>
+</main>
+<?php
 	
 	// Load Footer
 	require "./assets/footer.php";
